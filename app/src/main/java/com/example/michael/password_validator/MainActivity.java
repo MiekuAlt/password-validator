b3f60a611dfe23b54edf1ca272b5dc1d40e4a457
Michael <m.altair@live.com>
2017-05-26 12:18:51 -0300

    Built the code to pass the tests
    All the tests are now passing

--- app/src/main/java/com/example/michael/password_validator/MainActivity.java
+++ app/src/main/java/com/example/michael/password_validator/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.michael.password_validator;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.KeyListener;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +16,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // This is run when the user validates the password
+    public void checkPW(View view) {
+        EditText editPW = (EditText) findViewById(R.id.editPassword);
+        Password password = new Password(editPW.getText().toString());
+        CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox);
+
+        ProgressBar progress = (ProgressBar) findViewById(R.id.progressBar);
+        progress.setProgress(password.rulesApproved);
+
+        if(password.approved) {
+            checkBox.setChecked(true);
+        } else {
+            checkBox.setChecked(false);
+        }
+    }
+
+
 }
