3467b5dc276ad0485f2654dff35360f34cc7230e
Michael <m.altair@live.com>
2017-05-25 21:02:45 -0300

    Restructuring Password.java
    Realized that the validator is required to return the number of passes
    that were successful. Corrected this early in the TDD and updated both
    branches a2-stage1 and a2-stage2

--- app/src/main/java/com/example/michael/password_validator/Password.java
+++ app/src/main/java/com/example/michael/password_validator/Password.java
@@ -7,11 +7,22 @@ package com.example.michael.password_validator;
 public class Password {
 
     String password;
+    int rulesApproved;
     boolean approved;
 
     public Password(String password) {
+        rulesApproved = validate(password);
+
+        // Determines if the password is approved, based on the number of passed rules
+        if(rulesApproved >= 0) { // Currently zero since we have no rules yet...
+            approved = true;
+        }
+    }
+
+    // Validates the password and returns the number of rules it has passed
+    public int validate(String password) {
         this.password = password;
-        approved = true;
+        return 0;
     }
 
 }
