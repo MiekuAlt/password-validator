8d8a4f79e239ed9dca2320368ec382af366c4191
Michael <m.altair@live.com>
2017-05-25 23:41:36 -0300

    Tests are now passing for stage 2
    Built the code to verify the additional rules for stage 3:
    - At least a digit
    - At least a symbol
    - Both uppercase and lowercase

--- app/src/main/java/com/example/michael/password_validator/Password.java
+++ app/src/main/java/com/example/michael/password_validator/Password.java
@@ -6,23 +6,68 @@ package com.example.michael.password_validator;
 
 public class Password {
 
-    String password;
-    int rulesApproved;
-    boolean approved;
+    private int MIN_LENGTH = 8; // The minimum length of the password
+    private int NUM_RULES = 5; // The number of rules required to pass for the password to be approved
+
+    public String password;
+    public int rulesApproved;
+    public boolean approved;
 
     public Password(String password) {
         rulesApproved = validate(password);
 
         // Determines if the password is approved, based on the number of passed rules
-        if(rulesApproved >= 0) { // Currently zero since we have no rules yet...
+        if(rulesApproved >= NUM_RULES) {
             approved = true;
         }
     }
 
     // Validates the password and returns the number of rules it has passed
-    public int validate(String password) {
+    private int validate(String password) {
+        int numPassed = 0;
         this.password = password;
-        return 0;
+
+        if(isNotPassword(password)) {
+            numPassed++;
+        }
+        if(isLongEnough(password)) {
+            numPassed++;
+        }
+        if(hasDigit(password)) {
+            numPassed++;
+        }
+        if(hasSymbol(password)) {
+            numPassed++;
+        }
+        if(hasBothCases(password)) {
+            numPassed++;
+        }
+        return numPassed;
     }
 
+    /****** The Rules for the Validator ******/
+    // Checking if the string is not "password"
+    private boolean isNotPassword(String password) {
+        return !password.toLowerCase().equals("password");
+    }
+
+    // Checks if the password is at least 8 chars long
+    private boolean isLongEnough(String password) {
+        return password.length() >= MIN_LENGTH;
+    }
+
+    // Checks if there is at least a digit in the password
+    private boolean hasDigit(String password) {
+        return password.matches(".*\\d+.*");
+    }
+
+    // Checks if there is at lease a symbol in the password
+    private boolean hasSymbol(String password) {
+        return password.matches(".*[^A-Za-z0-9]+.*");
+    }
+
+    // Checks if there is both an uppercase and a lowercase char in the password
+    private boolean hasBothCases(String password) {
+        return (password.matches(".*[A-Z]+.*") && password.matches(".*[a-z]+.*"));
+    }
 }
