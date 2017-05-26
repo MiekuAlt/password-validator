39fdbf993e82c91e78d51ea50e3084d51b99cd7d
Michael <m.altair@live.com>
2017-05-25 21:23:27 -0300

    Tests now pass!
    The tests are now passing after building the code for the validator

--- app/src/main/java/com/example/michael/password_validator/Password.java
+++ app/src/main/java/com/example/michael/password_validator/Password.java
@@ -6,23 +6,42 @@ package com.example.michael.password_validator;
 
 public class Password {
 
-    String password;
-    int rulesApproved;
-    boolean approved;
+    public String password;
+    public int rulesApproved;
+    public boolean approved;
 
     public Password(String password) {
         rulesApproved = validate(password);
 
         // Determines if the password is approved, based on the number of passed rules
-        if(rulesApproved >= 0) { // Currently zero since we have no rules yet...
+        if(rulesApproved >= 2) { // Currently zero since we have no rules yet...
             approved = true;
         }
     }
 
     // Validates the password and returns the number of rules it has passed
-    public int validate(String password) {
+    private int validate(String password) {
+        int numPassed = 0;
         this.password = password;
-        return 0;
+
+        if(isNotPassword(password)) {
+            numPassed++;
+        }
+
+        if(isLongEnough(password)) {
+            numPassed++;
+        }
+
+        return numPassed;
     }
 
+    // Checking if the string is not "password"
+    private boolean isNotPassword(String password) {
+        return !password.toLowerCase().equals("password");
+    }
+
+    // Checks if the password is at least 8 chars long
+    private boolean isLongEnough(String password) {
+        return password.length() >= 8;
+    }
 }
